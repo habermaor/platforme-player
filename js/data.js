6947b9331deacb1c10047bd5526506b93b571fe5
Maor <habermaor@gmail.com>
2017-11-08 15:31:32 +0200

    snapiri eating maors

--- js/data.js
+++ js/data.js
@@ -6,7 +6,7 @@ module.exports =
         assets: {
             background: { key: "sky", url: "assets/sky.png" },
             ground: { key: "ground", url: "assets/platform.png" },
-            object: { key: "star", url: "assets/star.png" },
-            hero: { key: "dude", url: "assets/dude.png", frameWidth: 32, frameHeight:48 }
+            object: { key: "star", url: "assets/me.png" },
+            hero: { key: "dude", url: "assets/snap.png", frameWidth: 48, frameHeight:48 }
         }
     }
