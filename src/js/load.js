ccbbdf9cf84c50e9115aa948b2010f5eeeb81583
Maor <habermaor@gmail.com>
2018-05-09 14:34:45 +0300

    call api with relative url

--- src/js/load.js
+++ src/js/load.js
@@ -5,10 +5,9 @@
         var defauldData = this.defauldData;
         var p = (window.location.pathname || '').split('/');
         let getGame = new Promise((resolve, reject) => {
-            console.log(p, p[1])
             this.game.data = defauldData;
             const xhr = new XMLHttpRequest();            
-            xhr.open("GET", 'http://localhost:3000/api/item/' + p[1]);
+            xhr.open("GET", '/api/item/' + p[1]);
             xhr.onload = () => resolve(xhr.responseText);
             xhr.onerror = () => { this.game.data = defauldData; console.log('wtf', this.game.data); this.game.state.start('play'); reject(xhr.statusText); }
             xhr.send();
